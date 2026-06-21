963f79e5f3d625af3f8f98aaa82977359d4da1ca
kravecmaria46-cmd <kravecmaria46@gmail.com>
2026-06-22 01:18:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5080,5 +5080,5 @@ async def serve_frontend():
     return HTMLResponse(HTML)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 10000))
     uvicorn.run(app, host="0.0.0.0", port=port)
